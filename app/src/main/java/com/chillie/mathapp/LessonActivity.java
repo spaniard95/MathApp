e695eb3748d60e97ac85924703dfca1523228943
spaniard95 <panagiotis.spanos95@gmail.com>
2020-03-16 23:57:04 +0200

    lessonc xml progressed started lesson acitvity early

--- app/src/main/java/com/chillie/mathapp/LessonActivity.java
+++ app/src/main/java/com/chillie/mathapp/LessonActivity.java
@@ -3,8 +3,14 @@ package com.chillie.mathapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LessonActivity extends AppCompatActivity {
+private TextView mLessonTitle,mLessonCategory;
+private CheckBox mLessonStar;
+private EditText mInsertGrade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
